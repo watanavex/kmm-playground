498dbac65118b3023e7ad715639ed9dd8c012365
watanavex <y_watanabe@yumemi.co.jp>
2021-03-13 14:28:32 +0900

    Remove template files

--- model/src/androidMain/kotlin/tech/watanave/kmm_playground/model/Platform.kt
+++
@@ -1,5 +0,0 @@
-package tech.watanave.kmm_playground.model
-
-actual class Platform actual constructor() {
-    actual val platform: String = "Android ${android.os.Build.VERSION.SDK_INT}"
-}
--- model/src/commonMain/kotlin/tech/watanave/kmm_playground/model/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package tech.watanave.kmm_playground.model
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
