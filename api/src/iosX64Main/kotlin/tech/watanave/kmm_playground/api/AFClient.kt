de28913fe317a0b23076bbf5e89038a5d43a6321
watanavex <y_watanabe@yumemi.co.jp>
2021-03-24 10:37:37 +0900

    Link check for AFNetworking

---
+++ api/src/iosX64Main/kotlin/tech/watanave/kmm_playground/api/AFClient.kt
@@ -0,0 +1,11 @@
+package tech.watanave.kmm_playground.api
+
+import cocoapods.AFNetworking.*
+
+class AFClient {
+
+    fun request(searchWord: String) {
+        val i = AFNetworkReachabilityStatus.MAX_VALUE
+    }
+
+}
