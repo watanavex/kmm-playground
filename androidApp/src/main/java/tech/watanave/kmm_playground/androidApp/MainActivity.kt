02da6c23eb5cdc6f158a43582582ffdf9537ba1e
watanavex <y_watanabe@yumemi.co.jp>
2021-03-30 11:34:41 +0900

    Apply koin to android app

--- androidApp/src/main/java/tech/watanave/kmm_playground/androidApp/MainActivity.kt
+++ androidApp/src/main/java/tech/watanave/kmm_playground/androidApp/MainActivity.kt
@@ -1,24 +1,21 @@
 package tech.watanave.kmm_playground.androidApp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import org.koin.android.ext.android.inject
 import tech.watanave.kmm_playground.api.ApiClient
 
 class MainActivity : AppCompatActivity() {
+
+    private val api: ApiClient by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         lifecycleScope.launch {
-            val api = ApiClient()
             val response = api.request("Kotlin")
             println(response)
         }
